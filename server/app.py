f64ad15d62de2837f091217b7742770ec090c7f4
Mark <markjohnguto@gmail.com>
2026-01-04 22:01:26 +0300

    changes

--- server/app.py
+++ server/app.py
@@ -18,4 +18,6 @@ def index():
 
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(port=5555, debug=True)
--- server/testing/conftest.py
+++ server/testing/conftest.py
@@ -1,5 +1,30 @@
 #!/usr/bin/env python3
 
+import pytest
+from app import app, db
+
+
+@pytest.fixture(autouse=True, scope='function')
+def setup_database():
+    """Create database tables before each test and drop after."""
+    with app.app_context():
+        db.create_all()
+        yield
+        db.drop_all()
+
+
+def pytest_configure(config):
+    """Create all database tables once at the start of the test session."""
+    with app.app_context():
+        db.create_all()
+
+
+def pytest_unconfigure(config):
+    """Drop all database tables at the end of the test session."""
+    with app.app_context():
+        db.drop_all()
+
+
 def pytest_itemcollected(item):
     par = item.parent.obj
     node = item.obj
